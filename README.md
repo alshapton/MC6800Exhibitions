84648b4bab495febf463dbbb72634511554119fc
alshapton <alshapton@gmail.com>
2026-08-11 00:06:10 +0100

    Installed Sphinx infrastructure and started adding pages for VCF UK 2026

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-![MC6800 Logo](./images/generic/mc6800logo.jpeg)
+![MC6800 Logo](./docs/images/generic/mc6800logo.jpeg)
 
 
 # This repository contains information about the exhibitions which the MC6800 Collection has been to:
---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+
+project = 'Motorola MC6800 Exhibitions'
+copyright = '2026, Andrew Shapton'
+author = 'Andrew Shapton'
+release = '0.'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+        'sphinx.ext.autosectionlabel',
+        'sphinx_collapse',
+        'sphinx_design',
+        'linuxdoc.rstFlatTable',]
+#        
+
+autosectionlabel_prefix_document = True
+templates_path = ['_templates']
+exclude_patterns = []
+root_prefix = '/Users/andrew/MyProjects/Motorola-6800/MC6800Exhibitions'
+source_suffix = ['.rst']
+
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+    'https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Round',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'
+]
